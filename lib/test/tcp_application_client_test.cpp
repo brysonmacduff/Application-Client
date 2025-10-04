c15771124a7b475061ebfa3411096ff9acc1480a
JustBryse <brysonmacduff@gmail.com>
2025-10-04 12:33:05 -0400

    added client tx unit tests

--- lib/test/tcp_application_client_test.cpp
+++ lib/test/tcp_application_client_test.cpp
@@ -1,5 +1,6 @@
 #include "application_client.h"
 #include <gtest/gtest.h>
+#include <vector>
 
 namespace InterProcessCommunication::Test
 {
@@ -8,6 +9,7 @@ class TcpApplicationTest : public ::testing::Test
 {
 public:
     static constexpr std::chrono::milliseconds CLIENT_STATE_POLL_INTERVAL { 10 };
+    static constexpr int BUFFER_SIZE = 1024;
     const std::string IPV4_ADDRESS { "127.0.0.1" };
     const uint16_t PORT = 5000;
     ApplicationClient m_client {IPV4_ADDRESS, PORT};
@@ -48,6 +50,67 @@ public:
 
             std::cout << "TCP_SERVER -> Accepted client connection: " << count+1 << "\n";
         }
+
+        // Wait until signalled by the test case to close the server
+        server_shutdown_semaphore.acquire();
+
+        close(m_client_file_descriptor);
+        close(m_server_file_descriptor);
+
+        std::cout << "TCP_SERVER -> Server has shutdown.\n";
+    }
+
+    void StartMessageReceiverTcpServer(std::binary_semaphore& server_running_semaphore, std::binary_semaphore& server_done_semaphore, std::binary_semaphore& server_shutdown_semaphore, std::string expected_payload)
+    {
+        const int connection_limit = 1;
+        const int m_server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+        EXPECT_NE(m_server_file_descriptor, -1);
+        sockaddr_in address{};
+
+        // Force the port to be freed after use by the server
+        const int server_socket_option = 1;
+        setsockopt(m_server_file_descriptor, SOL_SOCKET, SO_REUSEADDR, &server_socket_option, sizeof(server_socket_option));
+  
+        address.sin_family = AF_INET;
+        inet_pton(AF_INET, IPV4_ADDRESS.c_str(), &address.sin_addr);
+        address.sin_port = htons(PORT);
+
+        EXPECT_NE(bind(m_server_file_descriptor, (sockaddr*)&address, sizeof(address)),-1);
+        EXPECT_NE(listen(m_server_file_descriptor, connection_limit),-1);
+
+        // Signal to the test case that the server is ready to accept a connection
+        server_running_semaphore.release();
+
+        std::cout << "TCP_SERVER -> Server is listening for connection attempts...\n";
+
+        const int m_client_file_descriptor = accept(m_server_file_descriptor, nullptr, nullptr);
+
+        EXPECT_NE(m_client_file_descriptor, -1);
+
+        std::cout << "TCP_SERVER -> Accepted client connection.\n";
+
+        std::string received_payload;
+
+        while(received_payload.size() < expected_payload.size())
+        {
+            std::vector<char> buffer(BUFFER_SIZE);
+            std::span<char> buffer_view(buffer);
+
+            const ssize_t bytes = read(m_client_file_descriptor, buffer_view.data(), buffer_view.size());
+
+            EXPECT_GT(bytes, -1);
+
+            const std::string received_message (buffer_view.data(), bytes);
+
+            std::cout << "TCP_SERVER -> Received message: {"+received_message+"}\n";
+
+            received_payload += received_message;
+        }
+
+        EXPECT_EQ(received_payload, expected_payload);
+
+        // signal the client that the server is done reading messages
+        server_done_semaphore.release();
 
         // Wait until signalled by the test case to close the server
         server_shutdown_semaphore.acquire();
@@ -157,6 +220,118 @@ TEST_F(TcpApplicationTest, ConnectAndDisconnectRepeatedly)
     std::cout << __func__ << " -> Releasing server!\n";
 
     // Signal the server that it is allowed to shutdown
+    server_shutdown_semaphore.release();
+
+    server_thread.join();
+}
+
+TEST_F(TcpApplicationTest, SendSingleMessage)
+{
+    std::string message = "hello there";
+
+    std::binary_semaphore server_running_semaphore(0);
+    std::binary_semaphore server_done_semaphore(0);
+    std::binary_semaphore server_shutdown_semaphore(0);
+    std::thread server_thread (&TcpApplicationTest::StartMessageReceiverTcpServer, this, std::ref(server_running_semaphore), std::ref(server_done_semaphore), std::ref(server_shutdown_semaphore), message);
+
+    EXPECT_TRUE(m_client.Start());
+
+    // Do not proceed until the client is ready to begin
+    while(not m_client.IsRunning())
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    // Wait here until the server signals it is ready
+    server_running_semaphore.acquire();
+
+    EXPECT_TRUE(m_client.RequestOpen());
+
+    std::cout << __func__ << " -> Requested open!\n";
+
+    while(m_client.GetClientState() != ClientState::CONNECTED)
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    EXPECT_EQ(m_client.GetClientState(), ClientState::CONNECTED);
+
+    const std::span<char> message_view (message);
+
+    EXPECT_TRUE(m_client.EnqueuePayload(message_view));
+
+    // wait for the server to signal that it is done reading
+    server_done_semaphore.acquire();
+
+    EXPECT_TRUE(m_client.RequestClose());
+
+    while(m_client.GetClientState() != ClientState::NOT_CONNECTED)
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    EXPECT_EQ(m_client.GetClientState(), ClientState::NOT_CONNECTED);
+
+    server_shutdown_semaphore.release();
+
+    server_thread.join();
+}
+
+TEST_F(TcpApplicationTest, SendMultipleMessages)
+{
+    const size_t message_count = 100;
+    std::vector<std::string> messages (message_count);
+    std::string total_payload;
+
+    for(size_t count = 0; count < message_count; ++count)
+    {
+        messages[count] = "<hello there " + std::to_string(count) + ">";
+        total_payload += messages[count];
+    }
+
+    std::binary_semaphore server_running_semaphore(0);
+    std::binary_semaphore server_done_semaphore(0);
+    std::binary_semaphore server_shutdown_semaphore(0);
+    std::thread server_thread (&TcpApplicationTest::StartMessageReceiverTcpServer, this, std::ref(server_running_semaphore), std::ref(server_done_semaphore), std::ref(server_shutdown_semaphore), total_payload);
+
+    EXPECT_TRUE(m_client.Start());
+
+    // Do not proceed until the client is ready to begin
+    while(not m_client.IsRunning())
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    // Wait here until the server signals it is ready
+    server_running_semaphore.acquire();
+
+    EXPECT_TRUE(m_client.RequestOpen());
+
+    while(m_client.GetClientState() != ClientState::CONNECTED)
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    EXPECT_EQ(m_client.GetClientState(), ClientState::CONNECTED);
+
+    for(std::string message : messages) 
+    {
+        const std::span<char> message_view (message);
+        EXPECT_TRUE(m_client.EnqueuePayload(message_view));
+    }
+
+    // wait for the server to signal that it is done reading
+    server_done_semaphore.acquire();
+
+    EXPECT_TRUE(m_client.RequestClose());
+
+    while(m_client.GetClientState() != ClientState::NOT_CONNECTED)
+    {
+        std::this_thread::sleep_for(CLIENT_STATE_POLL_INTERVAL);
+    }
+
+    EXPECT_EQ(m_client.GetClientState(), ClientState::NOT_CONNECTED);
+
     server_shutdown_semaphore.release();
 
     server_thread.join();
